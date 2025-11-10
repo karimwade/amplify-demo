b534388c9e16da3643965ca93a1b65de3a170b06
Papa Abdou Karim Wade <abdou.karim.info@gmail.com>
2025-11-10 17:59:58 +0000

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Test deploiement </h1>
+        <h1>Test deploiement 1 </h1>
       </header>
     </div>
   );
